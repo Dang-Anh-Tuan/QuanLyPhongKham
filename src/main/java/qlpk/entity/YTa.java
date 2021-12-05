315257e910aa49d0ec3df55614f3260790dc6bb3
Dang-Anh-Tuan <danganhtuan0002@gmail.com>
2021-12-05 12:22:27 +0700

    test valid bac sy yta

--- src/main/java/qlpk/entity/YTa.java
+++ src/main/java/qlpk/entity/YTa.java
@@ -1,8 +1,9 @@
 package qlpk.entity;
 
 import lombok.Data;
-
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -15,13 +16,17 @@ public class YTa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
+    @NotEmpty(message = "không được để trống")
     private String ten;
+    
     private String cmt;
     private String trinhDo;
     private int thamNien;
     private String diaChi;
     private String sdt;
-    @DateTimeFormat (pattern="dd/MM/yyyy")
+    @DateTimeFormat (pattern="yyyy-MM-dd")
+    @Past(message = "Ngày sinh phải trước ngày hiện tại")
     private Date bd;
     @OneToMany(targetEntity = BenhAn.class, mappedBy = "yTa")
     private Set<BenhAn> benhAn;
