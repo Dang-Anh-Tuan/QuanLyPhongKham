f782fb9749067d6fdb1073d4cc61ca34e987beea
namchtq238 <connorkenway238@gmail.com>
2021-12-10 13:42:38 +0700

    Update LoginController.java

--- src/main/java/qlpk/controller/LoginController.java
+++ src/main/java/qlpk/controller/LoginController.java
@@ -1,20 +1,17 @@
 package qlpk.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import qlpk.dto.auth.param.LoginFormParam;
 
 @Controller
 @RequestMapping({"/", "/login"})
 public class LoginController {
 	@GetMapping
-	public String showLoginForm() {
-		return "Login";
+	public String showDashBoard(){
+		return "404";
 	}
-	@PostMapping
-	public String postLoginForm(@ModelAttribute LoginFormParam loginFormParam){
-		System.out.println(loginFormParam.toString());
+	@GetMapping("/login")
+	public String showLoginForm() {
 		return "Login";
 	}
 }
