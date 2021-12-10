590022ba4bed60deb6a02808620c2299b81a7366
Dang-Anh-Tuan <danganhtuan0002@gmail.com>
2021-12-10 10:50:33 +0700

    12/10

--- src/main/java/qlpk/entity/YTa.java
+++ src/main/java/qlpk/entity/YTa.java
@@ -1,8 +1,14 @@
 package qlpk.entity;
 
 import lombok.Data;
+import qlpk.security.User;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 import java.util.Set;
 
@@ -12,13 +18,18 @@ public class YTa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
+    @NotEmpty(message = "không được để trống")
     private String ten;
+    
     private String cmt;
     private String trinhDo;
     private int thamNien;
     private String diaChi;
     private String sdt;
-    private Date ngaySinh;
+    @DateTimeFormat (pattern="yyyy-MM-dd")
+    @Past(message = "Ngày sinh phải trước ngày hiện tại")
+    private Date bd;
     @OneToMany(targetEntity = BenhAn.class, mappedBy = "yTa")
     private Set<BenhAn> benhAn;
     @OneToMany(targetEntity = ThongTinChamSoc.class, mappedBy = "yTa")
