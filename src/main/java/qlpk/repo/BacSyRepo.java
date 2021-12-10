825bb2d0bf7c84ce14170df0792dbcf25db0a256
Thịnh Trần Nguyên <84261217+thinh1407@users.noreply.github.com>
2021-12-10 14:33:39 +0700

    Update BacSyRepo.java

--- src/main/java/qlpk/repo/BacSyRepo.java
+++ src/main/java/qlpk/repo/BacSyRepo.java
@@ -2,8 +2,9 @@ package qlpk.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 import qlpk.entity.BacSy;
 
 import java.util.Date;
@@ -13,6 +14,6 @@ import java.util.Optional;
 public interface BacSyRepo extends JpaRepository<BacSy, Integer> {
     @Query("SELECT bs from BacSy bs where bs.cmt = ?1")
     public BacSy findByCmt(String cmt);
-    @Query(value = "CALL `demo-stored-procedure`.get_salary_doctor(:did, :sdate);", nativeQuery = true)
-    public float tinhLuongBacSy(@Param("did") int did, @Param("sdate") Date sdate);
+    @Query(value = "CALL get_salary_doctor(:did, :sdate);", nativeQuery = true)
+    public float tinhLuongBacSy(@RequestParam("did") int did, @RequestParam("sdate") Date sdate);
 }
